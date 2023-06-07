c13433095d65d851f694f9d45a6a6dff3f7f82bd
JJStephens <siahstephens@gmail.com>
2023-06-07 10:35:35 -0600

    fixed bugs

--- js/index.js
+++ js/index.js
@@ -1,20 +1,3 @@
 function clearForm() {
   document.getElementById("campaign-form").reset();
 }
-
-function validateEmail(email) {
-  const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-  return regex.test(email);
-}
-
-// validate age
-function validateAge(ageText) {
-  const regex = /^(?:1[0-1][0-9]|120|[1-9][0-9]?)$/;
-  return regex.test(ageText);
-}
-
-// validate zipcode
-function validateZipCode(zipcodeText) {
-  const regex = /^[0-9]{5}$/;
-  return regex.test(zipcodeText);
-}
